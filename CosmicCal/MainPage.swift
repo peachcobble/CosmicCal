5db18e779e63daf95f2b061cffc59154d393e2cc
BT_C09_14 <Hp5680@bths.edu>
2026-05-05 14:34:55 -0400

    General improvs

---
+++ CosmicCal/MainPage.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to CosmicCal")
+                .font(.title)
+                .padding(.top, 50)
+            Spacer()
+        }
+    }
+}
