3f06820f9b564cc612de2e72cc17977a8a6a3a9a
BT_C09_14 <Hp5680@bths.edu>
2026-05-05 14:14:08 -0400

    Initial Commit

---
+++ CosmicCal/CosmicCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmicCalApp.swift
+//  CosmicCal
+//
+//  Created by Student on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CosmicCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
