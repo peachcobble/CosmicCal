bfedaf7478a89e2ac9a0d8a41f46481a92be1483
irisli <schools@gmail.com>
2026-06-12 00:55:15 -0400

    UI changes and fixed bugs

--- CosmicCal/MainPage.swift
+++ CosmicCal/MainPage.swift
@@ -10,140 +10,124 @@ struct MainPage: View {
     var body: some View {
         NavigationStack {
             ZStack {
-               Color(.tintColor)
-                  .ignoresSafeArea()
-                VStack {
-                    Text(networkClient.NASAOfTheDay.title)
-                        .font(.title)
-                    Spacer()
-                        .frame(height: 10)
-                    Text("Date: \(networkClient.NASAOfTheDay.date)")
-                    HStack {
-                        Text("CosmicCal")
-                            .fontWeight(.bold)
-                            .font(.largeTitle)
-                            .foregroundStyle(.white)
-                        Spacer()
-                        NavigationLink(destination: CalendarPage(date: $selectedDate)) {
-                            Image(systemName: calendarIcon)
-                                .tint(.white)
-                                .frame(maxWidth: 45, maxHeight: 45)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .tint(.accentColor)
-                                        .opacity(0.25)
-                                        .brightness(-2)
-                                }
+                Color(.tintColor)
+                    .ignoresSafeArea()
+                ScrollView {
+                    VStack(spacing: 20) {
+                        HStack {
+                            Text("CosmicCal")
+                                .fontWeight(.bold)
                                 .font(.largeTitle)
-                        }
-                    }
-                    VStack {
-                        Text(networkClient.NASAOfTheDay.title)
-                            .font(.title)
-                        Spacer()
-                            .frame(height: 10)
-                        Text("Date: \(networkClient.NASAOfTheDay.date)")
-                        if let imageURL = URL(string: networkClient.NASAOfTheDay.hdurl) {
-                            AsyncImage(url: imageURL) { receivedImage in
-                                receivedImage
-                                    .resizable()
-                                    .scaledToFit()
-                            } placeholder: {
-                                ProgressView()
+                                .foregroundStyle(.white)
+                            Spacer()
+                            NavigationLink(destination: CalendarPage(date: $selectedDate)) {
+                                Image(systemName: calendarIcon)
+                                    .tint(.white)
+                                    .frame(width: 45, height: 45)
+                                    .background {
+                                        RoundedRectangle(cornerRadius: 12)
+                                            .tint(.accentColor)
+                                            .opacity(0.25)
+                                            .brightness(-2)
+                                    }
+                                    .font(.largeTitle)
                             }
-                            .frame(height: 250)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .shadow(color: .black, radius: 15)
-                            .padding(.top)
-                        } else {
-                            Image(systemName: "photo")
-                                .font(.system(size: 120, weight: .ultraLight))
-                                .frame(width: 150, height: 200)
-                                .background(.blue)
-                                .shadow(color: .black, radius: 15)
-                                .cornerRadius(15)
-                                .padding(.top)
                         }
-                        Text(networkClient.NASAOfTheDay.explanation)
-                        Spacer()
-                    }
-                    .frame(width: .infinity, height: 200)
-                }
-                .padding()
-                .frame(width: .infinity)
-                .background{
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundStyle(.tint)
-                        .brightness(-0.15)
-                }
-                
-                VStack {
-                    Text("Moon Phase")
-                        .font(.title)
-                    HStack {
-                        if let imageURL = URL(string: networkClient.moonPhase.image.url) {
-                            AsyncImage(url: imageURL) { receivedImage in
-                                receivedImage
-                                    .resizable()
-                                    .scaledToFit()
-                            } placeholder: {
-                                ProgressView()
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(networkClient.NASAOfTheDay.title)
+                                .font(.title)
+                                .bold()
+                            
+                            Text("Date: \(networkClient.NASAOfTheDay.date)")
+                                .font(.subheadline)
+                            
+                            if let imageURL = URL(string: networkClient.NASAOfTheDay.hdurl) {
+                                AsyncImage(url: imageURL) { receivedImage in
+                                    receivedImage
+                                        .resizable()
+                                        .scaledToFit()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(maxHeight: 250)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .shadow(color: .black.opacity(0.3), radius: 15)
+                            } else {
+                                Image(systemName: "photo")
+                                    .font(.system(size: 120, weight: .ultraLight))
+                                    .frame(maxWidth: .infinity, minHeight: 200)
+                                    .background(.blue)
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                                    .shadow(color: .black.opacity(0.3), radius: 15)
                             }
-                            .frame(height: 150)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .shadow(color: .black, radius: 15)
-                            .padding(.top)
+                            
+                            Text(networkClient.NASAOfTheDay.explanation)
+                                .font(.body)
                         }
-                        else {
-                            Image(systemName: "photo")
-                                .font(.system(size: 120, weight: .ultraLight))
-                                .frame(width: 150, height: 200)
-                                .background(.blue)
-                                .shadow(color: .black, radius: 15)
-                                .cornerRadius(15)
-                                .padding(.top)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundStyle(.tint)
+                                .brightness(-0.15)
                         }
-                        Text(networkClient.getMoonDescription(phase: networkClient.moonPhase.phase, age: networkClient.moonPhase.age))
-                            .font(.subheadline)
-                            .frame(height: 150)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Moon Phase")
+                                .font(.title)
+                                .bold()
+                            
+                            HStack(spacing: 15) {
+                                if let imageURL = URL(string: networkClient.moonPhase.image.url) {
+                                    AsyncImage(url: imageURL) { receivedImage in
+                                        receivedImage
+                                            .resizable()
+                                            .scaledToFit()
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    .frame(maxHeight: 150)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .shadow(color: .black.opacity(0.3), radius: 15)
+                                } else {
+                                    Image(systemName: "photo")
+                                        .font(.system(size: 80, weight: .ultraLight))
+                                        .frame(width: 120, height: 120)
+                                        .background(.blue)
+                                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                                        .shadow(color: .black.opacity(0.3), radius: 15)
+                                }
+                                
+                                Text(networkClient.getMoonDescription(phase: networkClient.moonPhase.phase, age: networkClient.moonPhase.age))
+                                    .font(.subheadline)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundStyle(.tint)
+                                .brightness(-0.15)
+                        }
                     }
-                }
-                .padding()
-                .frame(width: .infinity, height: 250)
-                .background{
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundStyle(.tint)
-                        .brightness(-0.15)
+                    .padding()
                 }
             }
             .task {
                 try? await networkClient.getAPOD(date: selectedDate)
                 try? await networkClient.getMoonPhase(date: selectedDate)
             }
-            .onChange(of: selectedDate) {
+            .onChange(of: selectedDate) { _, newValue in
                 Task {
-                    try? await networkClient.getAPOD(date: selectedDate)
-                    try? await networkClient.getMoonPhase(date: selectedDate)
+                    try? await networkClient.getAPOD(date: newValue)
+                    try? await networkClient.getMoonPhase(date: newValue)
                 }
-                .onChange(of: selectedDate) {
-                    Task {
-                        try? await networkClient.getAPOD(date: selectedDate)
-                    }
-                }
-                .padding(30)
-    //            .background {
-    //                RoundedRectangle(cornerRadius: 12)
-    //                    .foregroundStyle(.tint)
-    //            }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .ignoresSafeArea()
             }
-            
         }
     }
 }
 
 #Preview {
     MainPage()
-        .environment(NetworkClient())
 }
